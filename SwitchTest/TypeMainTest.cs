b2a241744cdf1cb83338e1988c53b2737d1b5771
Aris <aris.sektor@gmail.com>
2018-12-10 16:49:39 +0300

    [updated] tests

--- SwitchTest/TypeMainTest.cs
+++ SwitchTest/TypeMainTest.cs
@@ -75,42 +75,156 @@ namespace SwitchTest
         [InlineData(new int[] { (int)IntConst.INT1, (int)IntConst.INT2, (int)IntConst.INT3 })]
         public void IntTest(in int[] intArray)
         {
+            _switchValInt = intArray
+                .Concat(new int[] { -2000000000, 2000000000 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<int>()
+                .FirstOrDefault();
 
+            var type = _switchValInt
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(int), _switchValInt.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(int)));
+            Assert.True(type.IsValueType);
+
+            _switchValInt
+                .CaseOf((int)IntConst.INT1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf((int)IntConst.INT2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf((int)IntConst.INT3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
         [InlineData(new long[] { (long)LongConst.LONG1, (long)LongConst.LONG2, (long)LongConst.LONG3 })]
         public void LongTest(in long[] longArray)
         {
+            _switchValLong = longArray
+                .Concat(new long[] { -9000000000000000000, 9000000000000000000 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<long>()
+                .FirstOrDefault();
 
+            var type = _switchValLong
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(long), _switchValLong.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(long)));
+            Assert.True(type.IsValueType);
+
+            _switchValLong
+                .CaseOf((long)LongConst.LONG1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf((long)LongConst.LONG2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf((long)LongConst.LONG3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
         [InlineData(new byte[] { (byte)ByteConst.BYTE1, (byte)ByteConst.BYTE2, (byte)ByteConst.BYTE3 })]
         public void ByteTest(in byte[] byteArray)
         {
+            _switchValByte = byteArray
+                .Concat(new byte[] { 1, 222 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<byte>()
+                .FirstOrDefault();
 
+            var type = _switchValByte
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(byte), _switchValByte.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(byte)));
+            Assert.True(type.IsValueType);
+
+            _switchValByte
+                .CaseOf((byte)ByteConst.BYTE1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf((byte)ByteConst.BYTE2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf((byte)ByteConst.BYTE3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
         [InlineData(new ushort[] { (ushort)UshortConst.USHORT1, (ushort)UshortConst.USHORT2, (ushort)UshortConst.USHORT3 })]
         public void UshortTest(in ushort[] ushortArray)
         {
+            _switchValUshort = ushortArray
+                .Concat(new ushort[] { 1, 65000 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<ushort>()
+                .FirstOrDefault();
 
+            var type = _switchValUshort
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(ushort), _switchValUshort.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(ushort)));
+            Assert.True(type.IsValueType);
+
+            _switchValUshort
+                .CaseOf((ushort)UshortConst.USHORT1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf((ushort)UshortConst.USHORT2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf((ushort)UshortConst.USHORT3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
         [InlineData(new uint[] { (uint)UintConst.UINT1, (uint)UintConst.UINT2, (uint)UintConst.UINT3 })]
         public void UintTest(in uint[] uintArray)
         {
+            _switchValUint = uintArray
+                .Concat(new uint[] { 1, 4000000000 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<uint>()
+                .FirstOrDefault();
 
+            var type = _switchValUint
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(uint), _switchValUint.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(uint)));
+            Assert.True(type.IsValueType);
+
+            _switchValUint
+                .CaseOf((uint)UintConst.UINT1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf((uint)UintConst.UINT2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf((uint)UintConst.UINT3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
         [InlineData(new ulong[] { (ulong)UlongConst.ULONG1, (ulong)UlongConst.ULONG2, (ulong)UlongConst.ULONG3 })]
         public void UlongTest(in ulong[] ulongArray)
         {
+            _switchValUlong = ulongArray
+                .Concat(new ulong[] { 1, 18446744073709551615 - 1 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<ulong>()
+                .FirstOrDefault();
 
+            var type = _switchValUlong
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(ulong), _switchValUlong.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(ulong)));
+            Assert.True(type.IsValueType);
+
+            _switchValUlong
+                .CaseOf((ulong)UlongConst.ULONG1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf((ulong)UlongConst.ULONG2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf((ulong)UlongConst.ULONG3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
@@ -147,14 +261,31 @@ namespace SwitchTest
             FloatConst.FLOAT1,
             FloatConst.FLOAT2,
             FloatConst.FLOAT3,
-            FloatConst.FLOAT4,
-            FloatConst.FLOAT5,
-            FloatConst.FLOAT6,
-            FloatConst.FLOAT7
+            FloatConst.FLOAT4
         })]
         public void FloatTest(in float[] floatArray)
         {
+            _switchValFloat= floatArray
+                .Concat(new float[] { FloatConst.FLOAT5, FloatConst.FLOAT6, FloatConst.FLOAT7 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<float>()
+                .FirstOrDefault();
 
+            var type = _switchValFloat
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(float), _switchValFloat.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(float)));
+            Assert.True(type.IsValueType);
+
+            _switchValFloat
+                .CaseOf(FloatConst.FLOAT1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf(FloatConst.FLOAT2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf(FloatConst.FLOAT3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .CaseOf(FloatConst.FLOAT4).Accomplish(v => _output.WriteLine($"Fourth value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
@@ -162,14 +293,31 @@ namespace SwitchTest
             DoubleConst.DOUBLE1,
             DoubleConst.DOUBLE2,
             DoubleConst.DOUBLE3,
-            DoubleConst.DOUBLE4,
-            DoubleConst.DOUBLE5,
-            DoubleConst.DOUBLE6,
-            DoubleConst.DOUBLE7
+            DoubleConst.DOUBLE4
         })]
         public void DoubleTest(in double[] doubleArray)
         {
+            _switchValDouble = doubleArray
+                .Concat(new double[] { DoubleConst.DOUBLE5, DoubleConst.DOUBLE6, DoubleConst.DOUBLE7 })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<double>()
+                .FirstOrDefault();
 
+            var type = _switchValDouble
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(double), _switchValDouble.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(double)));
+            Assert.True(type.IsValueType);
+
+            _switchValDouble
+                .CaseOf(DoubleConst.DOUBLE1).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf(DoubleConst.DOUBLE2).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf(DoubleConst.DOUBLE3).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .CaseOf(DoubleConst.DOUBLE4).Accomplish(v => _output.WriteLine($"Fourth value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
@@ -183,14 +331,27 @@ namespace SwitchTest
         [InlineData(new bool[] { BooleanConst.BOOL1, BooleanConst.BOOL2 })]
         public void BoolTest(in bool[] boolArray)
         {
+            _switchValBool = boolArray
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<bool>()
+                .FirstOrDefault();
 
+            var type = _switchValBool
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(bool), _switchValBool.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(bool)));
+            Assert.True(type.IsValueType);
+
+            _switchValBool
+                .CaseOf(BooleanConst.BOOL2).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         [Theory]
         [InlineData(new TypeCode[] {
-            TypeCode.Empty,
-            TypeCode.Object,
-            TypeCode.DBNull,
             TypeCode.Boolean,
             TypeCode.Char,
             TypeCode.SByte,
@@ -203,12 +364,45 @@ namespace SwitchTest
             TypeCode.Single,
             TypeCode.Double,
             TypeCode.Decimal,
-            TypeCode.DateTime,
-            TypeCode.String
+            TypeCode.DateTime            
         })]
         public void EnumTest(in TypeCode[] enumArray)
         {
+            _switchValEnum = enumArray
+                .Concat(new TypeCode[] {
+                    TypeCode.Empty,
+                    TypeCode.Object,
+                    TypeCode.DBNull,
+                    TypeCode.String,
+                    TypeCode.DateTime
+                })
+                .OrderBy(z => Guid.NewGuid())
+                .Cast<TypeCode>()
+                .FirstOrDefault();
 
+            var type = _switchValEnum
+                .GetType()
+                .GetGenericArguments()
+                .SingleOrDefault();
+
+            Assert.StrictEqual(typeof(TypeCode), _switchValEnum.SwitchValue.GetType());
+            Assert.True(type.IsEquivalentTo(typeof(TypeCode)));
+            Assert.True(type.IsValueType);
+
+            _switchValEnum
+                .CaseOf(TypeCode.Boolean).Accomplish(v => _output.WriteLine($"First value: {v}"))
+                .CaseOf(TypeCode.Char).Accomplish(v => _output.WriteLine($"Second value: {v}"))
+                .CaseOf(TypeCode.SByte).Accomplish(v => _output.WriteLine($"Third value: {v}"))
+                .CaseOf(TypeCode.Byte).Accomplish(v => _output.WriteLine($"Fourth value: {v}"))
+                .CaseOf(TypeCode.Int16).Accomplish(v => _output.WriteLine($"Fifth value: {v}"))
+                .CaseOf(TypeCode.UInt16).Accomplish(v => _output.WriteLine($"Sixth value: {v}"))
+                .CaseOf(TypeCode.Int32).Accomplish(v => _output.WriteLine($"Seventh value: {v}"))
+                .CaseOf(TypeCode.Int64).Accomplish(v => _output.WriteLine($"Eighth value: {v}"))
+                .CaseOf(TypeCode.UInt64).Accomplish(v => _output.WriteLine($"Ninth value: {v}"))
+                .CaseOf(TypeCode.Single).Accomplish(v => _output.WriteLine($"Tenth value: {v}"))
+                .CaseOf(TypeCode.Double).Accomplish(v => _output.WriteLine($"Eleventh value: {v}"))
+                .CaseOf(TypeCode.Decimal).Accomplish(v => _output.WriteLine($"Twelfth value: {v}"))
+                .ChangeOverToDefault.Accomplish(vDef => _output.WriteLine($"Default value: {vDef}"));
         }
 
         #endregion
